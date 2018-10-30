6f3cbd449d243284e93547b0f5f883b4a1339779
ChidiNwaka <chidi.nwaka@gmail.com>
2018-10-30 17:19:22 -0400

    Updated ReadME.md

--- setup_netrc_file.py
+++ setup_netrc_file.py
@@ -1,53 +1,18 @@
 #!/usr/bin/python
-
-import os, sys
+import os, argparse
 from os import path
 from os.path import expanduser
+import shutil
 
-MACHINE = "labkey.uhnresearch.ca"
-LOGIN = ""
-PASSWORD = ""
 HOME = expanduser("~")
 
-# Two options to log in:
-# 1. with the user's email address and password eg. python setup.py user@labkey.org mypassword
-# 2. with an apikey eg. python setup.py apikey "apikey|8f28f0423434323412cbaw45242342342db6a4"
 
-if len(sys.argv) == 3:
-    LOGIN = sys.argv[1]
-    PASSWORD = sys.argv[2]
+def get_options():
+    parser = argparse.ArgumentParser(description="Storing labkey credentials in a netrc file")
+    parser.add_argument("-u", "--user_email", type=str, required=True, help="input project name on Labkey")
+    parser.add_argument("-p", "--user_password", type=str, required=True, help="output file name including path")
 
-# python setup.py apikey apikey|c4716e2076b57b1dce5d7a5f3c7d2d8a
-# LOGIN = "apikey"
-# PASSWORD = "apikey|c4716e2076b57b1dce5d7a5f3c7d2d8a"
-
-def main():
-    try:
-        import labkey  # Checking if LabKey module is installed.
-    except ImportError:
-        print 'Error, Module ModuleName is required'
-
-    if os.name == 'nt':  # for window machines
-        netrc_file = "_netrc"
-    else:
-        netrc_file = ".netrc"
-
-    # The file should be located in your home directory
-    filename = HOME + "/" + netrc_file
-
-    # If file exists, copy every line that doesn't have labkey info to temp file, delete filename and replace it.
-    if path.exists(filename):
-        remove_labkey_info(filename, netrc_file)
-
-    file = open(filename, "a")
-
-    output_line = "machine " + MACHINE + "\t" + "login " + LOGIN + "\t" + "password " + PASSWORD + "\n"
-    file.write(output_line)
-
-    file.close()
-
-    # Grant file's full permission to only the user.
-    os.chmod(filename, 0o700)
+    return parser.parse_args()
 
 
 def remove_labkey_info(filename, netrc_file):
@@ -59,14 +24,45 @@ def remove_labkey_info(filename, netrc_file):
                 tempFile.write(line)
 
     os.remove(filename)
+
+    filename = HOME + "/" + netrc_file
+    shutil.copy(filename_temp, filename)
+
+    return filename
+
+
+def main():
+    MACHINE = "labkey.uhnresearch.ca"
+    args = get_options()
+    user_email = args.user_email
+    user_password = args.user_password
+
+    try:
+        import labkey  # Checking if labkey module is installed.
+    except ImportError:
+        print 'Error, Module ModuleName is required'
+
+    if os.name == 'nt':  # for window machines.
+        netrc_file = "_netrc"
+    else:
+        netrc_file = ".netrc"
+
+    # The file should be located in your home directory.
     filename = HOME + "/" + netrc_file
 
-    with open(filename_temp, 'r') as tempFile:
-        with open(filename, 'w') as newfile:
-            for line in tempFile:
-                newfile.write(line)
+    # If file exists, copy every line that doesn't have labkey info to temp file, delete file and rename temp file.
+    if path.exists(filename):
+        filename = remove_labkey_info(filename, netrc_file)
 
-    os.remove(filename_temp)
+    file = open(filename, "a")
+
+    output_line = "machine " + MACHINE + "\t" + "login " + user_email + "\t" + "password " + user_password + "\n"
+    file.write(output_line)
+
+    file.close()
+
+    # Grant file's permission to only the user.
+    os.chmod(filename, 0o700)
 
 
 if __name__ == '__main__':
